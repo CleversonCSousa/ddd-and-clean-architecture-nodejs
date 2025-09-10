8b8e2f2f1662193302a928816d9643cb7e3a05c6
CleversonCSousa <cleversonsousa.git@gmail.com>
2025-09-10 11:52:22 -0300

    feat: add generation of fictitious question data

--- src/test/factories/make-question.ts
+++ src/test/factories/make-question.ts
@@ -1,15 +1,16 @@
 import { UniqueEntitityID } from "@/core/entities/unique-entity-id";
 import { Question, QuestionProps } from "@/domain/forum/enterprise/entities/question";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
+import { faker } from "@faker-js/faker";
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
+export function makeQuestion(override: Partial<QuestionProps> = {}, id?: UniqueEntitityID) {
     const question = Question.create({
-        title: "Example question",
+        title: faker.lorem.sentence(),
         slug: Slug.create("example-question"),
         authorId: new UniqueEntitityID(),
-        content: "Example content",
+        content: faker.lorem.text(),
         ...override
-    });
+    }, id);
 
     return question;
 }
